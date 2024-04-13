47061ad5364a40f9470633976543ca7931a83a2b
Kay Gosho <ketsume0211@gmail.com>
2024-04-14 01:25:01 +0900

    feat: enable to show win rate

--- pokemon_ai/q_learning/agent.py
+++ pokemon_ai/q_learning/agent.py
@@ -9,6 +9,10 @@ from pokemon_ai.q_learning.environment import Environment
 from pokemon_ai.simulator.battle import Battle
 from pokemon_ai.simulator.player import Action
 
+# TODO: enable to change them
+ALPHA = 0.1
+GAMMA = 0.9
+
 
 @dataclass
 class Experience:
@@ -24,21 +28,24 @@ class Agent:
         self.epsilon = epsilon
 
     def learn(self, env: Environment, episodes: int):
-        for e in range(episodes):
+        num_of_win = 0.0
+        for episode in range(episodes):
             state = env.reset()
             # print(state)
             experiences: List[Experience] = []
             while True:
-                # if e % 100 == 0:
-                #     env.render()
                 action = self.policy(state)
                 next_state, reward, terminated = env.step(action)
                 # print(state, reward, terminated)
                 state = next_state
                 experiences.append(Experience(state=state, action=action, reward=reward))
                 if terminated:
+                    if reward > 0:
+                        num_of_win += 1
                     break
-            print(experiences)
+            if episode % 100 == 0:
+                print("win rate:", num_of_win / (episode + 1))
+            # print(experiences)
             # TODO: monte-carlo learning
 
     def policy(self, state: ndarray):
